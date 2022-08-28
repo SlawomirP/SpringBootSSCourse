cbebfc2287661e702cc2905bdb4d009579ce6b0c
slawo <slawekpettke@gmail.com>
2022-08-28 18:54:09 +0200

    spring boot

--- src/main/java/plSlaw/autoconfig/MainApp.java
+++ src/main/java/plSlaw/autoconfig/MainApp.java
@@ -1,14 +1,32 @@
 package plSlaw.autoconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import plSlaw.autoconfig.configuration.AppConfiguration;
 import plSlaw.autoconfig.service.DocumentService;
+
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
+    //standardowa formułka przy aplikacjach spring boota
+        SpringApplication.run(MainApp.class, args);
+    }
 
+    //stworzenie instancji DocumentService i wstrzykniemy przez konstruktor
 
+    private DocumentService documentService;
+@Autowired
+    public MainApp(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println(documentService.generate());
     }
 }
